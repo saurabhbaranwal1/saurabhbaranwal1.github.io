f99dff20268f070eaed0ab8700c464d70a0adcc7
saurabhbaranwal1 <153089713+saurabhbaranwal1@users.noreply.github.com>
2023-12-06 18:31:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 18th!";
+  question.innerHTML = "Yay Tiya! I like you fr, see you for tomorrow's date! <3 ";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
